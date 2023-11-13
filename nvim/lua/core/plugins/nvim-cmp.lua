eda80650da901b818fd2bdefaa6934ba18e87fa5
pstet <123316024+pcs03@users.noreply.github.com>
2023-11-14 00:05:03 +0100

    Add load of plugins to new neovim config

---
+++ nvim/lua/core/plugins/nvim-cmp.lua
@@ -0,0 +1,42 @@
+return {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+        "hrsh7th/cmp-buffer", -- source for text in buffer
+        "hrsh7th/cmp-path", -- source for system paths
+        "L3MON4D3/LuaSnip", -- snippet engine
+        "saadparwaiz1/cmp_luasnip", -- for autocompletion
+        "rafamadriz/friendly-snippets", -- useful snippets
+    },
+    config = function()
+        local cmp = require("cmp")
+
+        local luasnip = require("luasnip")
+
+        -- Loads vscode styoe snippets from installed plugins
+        require("luasnip.loaders.from_vscode").lazy_load()
+
+        cmp.setup({
+            completion = {
+                completeopt = "menu,menuone,preview,noselect",
+            },
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<Tab>"] = cmp.mapping.confirm({ select = true })
+            }),
+            sources = cmp.config.sources({
+                { name = "buffer" }, -- text withing current buffer
+                { name = "path" }, -- file paths
+                { name = "luasnip" }, -- snippets
+            }),
+
+            
+        })
+    end,
+}
---
+++ nvim/lua/core/plugins/nvim-tree.lua
@@ -0,0 +1,56 @@
+return {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        local nvimtree = require("nvim-tree")
+
+        -- Settings from documentation
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+
+        -- Change color of tree arrows
+        vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
+
+        -- configure tree
+        nvimtree.setup({
+            view = {
+                width = 60,
+            },
+
+            -- Change folder arrow icons
+            renderer = {
+                indent_markers = {
+                    enable = true,
+                },
+                icons = {
+                    glyphs = {
+                        folder = {
+                          arrow_closed = "", -- arrow when folder is closed
+                          arrow_open = "", -- arrow when folder is open
+                        },
+                    },
+                },
+            },
+
+            actions = {
+                open_file = {
+                    window_picker = {
+                        enable = false,
+                    },
+                },
+            },
+            filters = {
+                custom = { ".DS_Store" },
+            },
+            git = {
+                ignore = false,
+            },
+        })
+
+        local keymap = vim.keymap
+
+        keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+    end
+}
