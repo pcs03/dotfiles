d67f8c0949c0771c0196891f98cd907b05e505be
pstet <123316024+pcs03@users.noreply.github.com>
2023-11-13 16:17:52 +0100

    Add more remaps for nvim

--- nvim/lua/custom/init.lua
+++ nvim/lua/custom/init.lua
@@ -3,9 +3,6 @@ vim.opt.colorcolumn = "100"
 vim.opt.relativenumber = true
 vim.opt.clipboard = ""
 
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 -- Remaps
@@ -16,3 +13,12 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+-- Paste without polluting the register
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Copy to system clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set("n", "Q", "<nop>")
