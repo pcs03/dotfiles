ac214a4d2e4b1dba9496d481b10ef2a122ef6c94
pstet <123316024+pcs03@users.noreply.github.com>
2024-02-05 22:22:44 +0100

    Add configs for hyprland and wofi

--- config/nvim/lua/core/plugins/lualine.lua
+++ config/nvim/lua/core/plugins/lualine.lua
@@ -6,7 +6,12 @@ return {
             options = {
                 theme = "horizon",
                 sections = {
+                    lualine_a = { "mode" },
+                    lualine_b = { "branch", "diff", "diagnostics" },
                     lualine_c = { "buffers" },
+                    lualine_x = { "encoding",  "fileformat", "filetype" },
+                    lualine_y = { "progress" },
+                    lualine_z = { "location" },
                 },
             },
         },
