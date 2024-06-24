6e4a5f2bed440f72de1856a807436cdaf55884e7
pstet <123316024+pcs03@users.noreply.github.com>
2024-06-24 12:08:58 +0200

    Add Gopls LSP, add more LSP keybinds

--- config/nvim/lua/core/plugins/lsp.lua
+++ config/nvim/lua/core/plugins/lsp.lua
@@ -19,9 +19,12 @@ return {
 		local lsp = require("lsp-zero")
 		lsp.preset("recommended")
 
+        local util = require("lspconfig/util")
+
 		lsp.setup_servers({})
 
 		local cmp = require("cmp")
+        local cmp_action = require('lsp-zero').cmp_action()
 		local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 		cmp.setup({
@@ -37,6 +40,8 @@ return {
 				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
 				["<C-y>"] = cmp.mapping.complete(),
 				["<Tab>"] = cmp.mapping.confirm({ select = true }),
+                ['<C-b>'] = cmp_action.luasnip_jump_forward(),
+                ['<C-f>'] = cmp_action.luasnip_jump_backward(),
 			}),
 		})
 
@@ -48,11 +53,29 @@ return {
 		})
 
 		lsp.on_attach(function(client, bufnr)
-			local opts = { buffer = bufnr, remap = false }
+			local opts = { buffer = bufnr}
 			local keymap = vim.keymap
 
 			opts.desc = "Show LSP definition"
 			keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+
+			opts.desc = "Go to declaration"
+			keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+
+			opts.desc = "Go to implementations"
+			keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<cr>", opts)
+
+			opts.desc = "Go to references"
+			keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>", opts)
+
+			opts.desc = "Go to type definition"
+			keymap.set("n", "go", "<cmd>Telescope lsp_type_definitions<cr>", opts)
+
+			opts.desc = "Show signiture information"
+			keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
+
+            opts.desc = "Show diagnostics"
+            keymap.set("n", "gl", vim.diagnostic.open_float, opts)
 
 			opts.desc = "Show documentation under cursor"
 			keymap.set("n", "K", vim.lsp.buf.hover, opts)
@@ -66,8 +89,12 @@ return {
             opts.desc = "Show code actions"
             keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
 
-            opts.desc = "Show diagnostics"
-            keymap.set("n", "gl", vim.diagnostic.open_float, opts)
+            opts.desc = "Move to previous diagnostic"
+            keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+
+            opts.desc = "Move to next diagnostic"
+            keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+
 		end)
 
 		require("mason").setup({})
@@ -93,11 +120,32 @@ return {
                 "texlab",
 			},
 			handlers = {
-				lsp.default_setup,
+                function(server_name)
+                    require('lspconfig')[server_name].setup({})
+                end,
+
+
 				lua_ls = function()
 					local lua_opts = lsp.nvim_lua_ls()
 					require("lspconfig").lua_ls.setup(lua_opts)
 				end,
+
+                gopls = function()
+                    require('lspconfig').gopls.setup({
+                        cmd = {"gopls"},
+                        filetypes = { "go", "gomod", "gowork", "gotmpl" },
+                        root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+                        settings = {
+                            gopls = {
+                                completeUnimported = true,
+                                usePlaceholders = true,
+                                analyses = {
+                                    unusedparams = true
+                                }
+                            }
+                        }
+                    })
+                end,
 			},
 		})
 	end,
