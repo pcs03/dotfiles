b116f757d666a2e02956429d8c5eb0588cf24c1f
pstet <123316024+pcs03@users.noreply.github.com>
2024-09-05 17:42:45 +0200

    Fix transparent background in nvim

--- config/nvim/lua/core/plugins/colorscheme.lua
+++ config/nvim/lua/core/plugins/colorscheme.lua
@@ -1,13 +1,11 @@
 return {
-    "folke/tokyonight.nvim",
-    "rose-pine/neovim",
-    "rebelot/kanagawa.nvim",
-    {
-        "catppuccin/nvim",
-        priority = 1000,
-        config = function()
-            vim.cmd([[colorscheme catppuccin]])
-        end,
-    }
+    "catppuccin/nvim",
+    priority = 1000,
+    config = function()
+        local cat = require("catppuccin")
+        cat.options.transparent_background = true
+        cat.compile()
+        vim.cmd([[colorscheme catppuccin]])
+    end,
 }
 
