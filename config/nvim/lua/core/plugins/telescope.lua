70a83f6984cdfa67641f84c6019a3f416c4f5506
pstet <123316024+pcs03@users.noreply.github.com>
2024-09-25 18:35:41 +0200

    Improve nvim telescope config

--- config/nvim/lua/core/plugins/telescope.lua
+++ config/nvim/lua/core/plugins/telescope.lua
@@ -1,32 +1,46 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-        "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-        local telescope = require("telescope")
-        local actions = require("telescope.actions")
+	"nvim-telescope/telescope.nvim",
+	dependencies = {
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			cond = function()
+				return vim.fn.executable("make") == 1
+			end,
+		},
+		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+		{ "nvim-telescope/telescope-ui-select.nvim" },
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
+		local themes = require("telescope.themes")
 
-        telescope.setup({
-            defaults = {
-                mappings = {
-                    i = {
-                        ["C-k"] = actions.move_selection_previous, -- Move to previous result
-                        ["C-j"] = actions.move_selection_next, -- Move to next result
-                        ["C-q"] = actions.send_selected_to_qflist + actions.open_qflist,
-                    }
-                }
-            }
-        })
+		telescope.setup({
+			extensions = {
+				["ui-select"] = {
+					themes.get_dropdown(),
+				},
+			},
+			defaults = {
+				mappings = {
+					i = {
+						["C-k"] = actions.move_selection_previous, -- Move to previous result
+						["C-j"] = actions.move_selection_next, -- Move to next result
+						["C-q"] = actions.send_selected_to_qflist + actions.open_qflist,
+					},
+				},
+			},
+		})
 
-        telescope.load_extension("fzf");
+		pcall(telescope.load_extension, "fzf")
+		pcall(telescope.load_extension, "ui-select")
 
-        local keymap = vim.keymap
+		local keymap = vim.keymap
 
-        keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-        keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-        keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-        keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-    end
+		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+	end,
 }
